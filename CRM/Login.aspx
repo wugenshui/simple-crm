68d2f6f5e65e06cebb7acf0535638cd2bcfbeba3
r720 <963838470@qq.com>
2018-03-25 19:34:57 +0800

    登陆页面新增

---
+++ CRM/Login.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="CRM.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>CRM客户管理系统</title>
+    <script src="js/jquery.js"></script>
+    <%-- 浮云效果 --%>
+    <script src="js/cloud.js"></script>
+</head>
+<body>
+    <div id="mainBody">
+        <div id="cloud1" class="cloud"></div>
+        <div id="cloud2" class="cloud"></div>
+    </div>
+    <div class="logintop">
+        <span>欢迎登录员工客户管理平台</span>
+        <ul>
+            <li><a href="#">客服</a></li>
+            <li><a href="#">帮助</a></li>
+            <li><a href="#">关于</a></li>
+        </ul>
+    </div>
+    <div class="loginbody">
+        <span class="systemlogo"></span>
+        <div class="loginbox">
+            <form id="loginform" runat="server">
+                <ul>
+                    <li>
+                        <input id="inputName" class="loginuser" runat="server" type="text" placeholder="用户名" /></li>
+                    <li>
+                        <input id="inputPassword" class="loginpwd" runat="server" type="password" placeholder="密码" /></li>
+                    <li>
+                        <asp:Button ID="Button1" type="submit" runat="server" class="loginbtn" Text="登录"
+                            OnClick="Button1_Click" Width="97px"></asp:Button>
+                        <label>
+                            <input name="" type="checkbox" value="" checked="checked" />记住密码</label><label><a href="#">忘记密码？</a></label>
+                    </li>
+                </ul>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ CRM/Login.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CRM
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        SqlConnection Connection = new SqlConnection();
+        Employee employee = new Employee();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Connection.ConnectionString = ConfigurationManager.ConnectionStrings["CRMConnection"].ConnectionString;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (CheckAccount(inputName.Value.ToString(), inputPassword.Value.ToString()))
+            {
+                Session["father"] = "";
+                Session["son"] = "";
+                Session["user"] = inputName.Value.ToString();
+                Session["password"] = inputPassword.Value.ToString();
+
+                Response.Redirect("~/MainFrm.aspx");
+                employee.EmployeeName = Session["user"].ToString();
+                employee.Password = inputPassword.Value.ToString();
+            }
+            else
+            { Response.Write("<script>alert('用户名或密码输入错误!')</script>"); }
+        }
+        private bool CheckAccount(string emp, string password)
+        {
+            bool checkStatus = false;
+            string strSQL = "Select * From emppassword where emp='" + emp + "' and Password='" + password + "'";
+            DataSet ds = new DataSet();
+            SqlDataAdapter adapter = new SqlDataAdapter(strSQL, Connection);
+
+            Connection.Open();
+            adapter.Fill(ds);
+
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                checkStatus = true;
+            }
+
+            return checkStatus;
+        }
+    }
+}
