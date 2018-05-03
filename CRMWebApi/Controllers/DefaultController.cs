b0ec72ac559291cb2a75a5d85a7c38f28a5ba73a
r720 <963838470@qq.com>
2018-05-03 22:16:45 +0800

    webapi项目新建

---
+++ CRMWebApi/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CRMWebApi.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult get()
+        {
+            return Json(123);
+        }
+    }
+}
