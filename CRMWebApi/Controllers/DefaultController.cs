f414ee2e835d66605647235fb4e015847b2c82fb
r720 <963838470@qq.com>
2018-05-03 22:37:46 +0800

    驼峰样式序列化处理

--- CRMWebApi/Controllers/DefaultController.cs
+++ CRMWebApi/Controllers/DefaultController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,10 +11,21 @@ namespace CRMWebApi.Controllers
 {
     public class DefaultController : ApiController
     {
+        public class Student
+        {
+            public string Name { get; set; }
+            public int age { get; set; }
+        }
+
         [HttpGet]
         public IHttpActionResult get()
         {
-            return Json(123);
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            // 驼峰式大小写
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            //settings.ContractResolver = new DefaultContractResolver();
+            Student student = new Student() { Name = "张三", age = 18 };
+            return Json(student, settings);
         }
     }
 }
