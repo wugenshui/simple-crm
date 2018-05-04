e4b94a4dab4bef512bb4aad3b701e0d720b47446
r720 <963838470@qq.com>
2018-05-04 23:16:48 +0800

    控制器序列化首字母小写

---
+++ CRMWebApi/Controllers/CompanyController.cs
@@ -0,0 +1,22 @@
+﻿using DAL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CRMWebApi.Controllers
+{
+    public class CompanyController : ApiController
+    {
+        public IHttpActionResult get()
+        {
+            Company com = new Company();
+            UserDAL _UserDAL = new UserDAL();
+            var users = _UserDAL.Get();
+            return Json(users);
+        }
+    }
+}
