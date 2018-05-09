9ea82f1c1ae3f1f73b81b7a286ea64993118ce03
r720 <963838470@qq.com>
2018-05-09 22:36:00 +0800

    安装依赖

---
+++ CRMWebApi/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using DAL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CRMWebApi.Controllers
+{
+    public class UserController : ApiController
+    {
+        public IHttpActionResult get()
+        {
+            Company com = new Company();
+            UserDAL _UserDAL = new UserDAL();
+            var users = _UserDAL.Get();
+            return Json(users);
+        }
+
+        public IHttpActionResult get(int id)
+        {
+            Company com = new Company();
+            UserDAL _UserDAL = new UserDAL();
+            var users = _UserDAL.Get().First(o => o.Id == id);
+            return Json(users);
+        }
+    }
+}
