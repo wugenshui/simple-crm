cc62b6e5ce1c92aa874c18ce3ad6d0df12b5bef5
r720 <963838470@qq.com>
2018-05-10 22:49:19 +0800

    登录页面

--- crm-webpack/src/App.vue
+++ crm-webpack/src/App.vue
@@ -10,6 +10,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+#app {
+  width: 100%;
+  height: 100%;
+}
 </style>
--- crm-webpack/src/page/login/Login.vue
+++ crm-webpack/src/page/login/Login.vue
@@ -1,43 +1,43 @@
 <template>
-    <div onselectstart="return false;">
-        <div class="logintop">
-            <span>欢迎登录员工客户管理平台</span>
-            <ul class="float-right">
-                <li>
-                    <a href="#">客服</a>
-                </li>
-                <li>
-                    <a href="#">帮助</a>
-                </li>
-                <li>
-                    <a href="#">关于</a>
-                </li>
-            </ul>
-        </div>
-        <div class="loginbody">
-            <div class="centerbox">
-                <span class="systemlogo"></span>
-                <div class="loginbox">
-                    <form id="loginform" runat="server">
-                        <ul>
-                            <li>
-                                <input id="inputName" class="loginuser" runat="server" type="text" placeholder="用户名" /></li>
-                            <li>
-                                <input id="inputPassword" class="loginpwd" runat="server" type="password" placeholder="密码" /></li>
-                            <li>
-                                <asp:Button ID="Button1" type="submit" runat="server" class="loginbtn" Text="登录" OnClick="Button1_Click" Width="97px"></asp:Button>
-                                <label>
-                                    <input name="" type="checkbox" value="" checked="checked" />记住密码</label>
-                                <label>
-                                    <a href="#">忘记密码？</a>
-                                </label>
-                            </li>
-                        </ul>
-                    </form>
-                </div>
-            </div>
-        </div>
+  <div onselectstart="return false;" class="main">
+    <div class="logintop">
+      <span>欢迎登录员工客户管理平台</span>
+      <ul class="float-right">
+        <li>
+          <a href="#">客服</a>
+        </li>
+        <li>
+          <a href="#">帮助</a>
+        </li>
+        <li>
+          <a href="#">关于</a>
+        </li>
+      </ul>
     </div>
+    <div class="loginbody">
+      <div class="centerbox">
+        <span class="systemlogo"></span>
+        <div class="loginbox">
+          <form id="loginform" runat="server">
+            <ul>
+              <li>
+                <input id="inputName" class="loginuser" runat="server" type="text" placeholder="用户名" /></li>
+              <li>
+                <input id="inputPassword" class="loginpwd" runat="server" type="password" placeholder="密码" /></li>
+              <li>
+                <button type="submit" class="loginbtn" @click="loginClick" Width="97px">登录</button>
+                <label>
+                  <input name="" type="checkbox" value="" checked="checked" />记住密码</label>
+                <label>
+                  <a href="#">忘记密码？</a>
+                </label>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -49,12 +49,13 @@ export default {
 </script>
 
 <style scoped>
-body {
+.main {
   background: url("../../image/login/light.png") no-repeat;
   background-size: cover;
   background-color: #1c77ac;
   position: relative;
   overflow: hidden;
+  height: 100%;
 }
 
 .logintop {
