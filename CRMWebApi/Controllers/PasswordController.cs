5047ef589a74cd033e20c87afe6c7b0185c543d1
963838470 <963838470@qq.com>
2018-05-17 10:46:36 +0800

    密码服务

---
+++ CRMWebApi/Controllers/PasswordController.cs
@@ -0,0 +1,28 @@
+﻿using Common;
+using DAL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CRMWebApi.Controllers
+{
+    public class PasswordController : ApiController
+    {
+        UserDAL _UserDAL = new UserDAL();
+
+        [HttpPut]
+        public IHttpActionResult change([FromBody]string password)
+        {
+            AjaxResult result = new AjaxResult();
+            User user = CommonHelper.User;
+            user.Password = MD5Helper.CreateMD5(password);
+            _UserDAL.changePassword(user);
+
+            return Json(result);
+        }
+    }
+}
--- DAL/UserDAL.cs
+++ DAL/UserDAL.cs
@@ -1,6 +1,8 @@
 ﻿using Model;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,15 @@ namespace DAL
 {
     public class UserDAL : BaseDAL<User>
     {
+        public User changePassword(User user)
+        {
+            DbContext db = GetDbContext();
+            DbEntityEntry<User> entry = db.Entry(user);
+            entry.State = EntityState.Unchanged;
+            entry.Property(t => t.Password).IsModified = true;
+
+            db.SaveChanges();
+            return user;
+        }
     }
 }
