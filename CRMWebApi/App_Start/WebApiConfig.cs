09acd38c60c75eac6bfe8b3b0037056db40908cd
wugenshui <963838470@qq.com>
2019-05-14 15:25:53 +0800

    全局异常

--- CRMWebApi/App_Start/WebApiConfig.cs
+++ CRMWebApi/App_Start/WebApiConfig.cs
@@ -22,6 +22,8 @@ namespace CRMWebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Filters.Add(new WebApiErrorHandleAttribute());
         }
     }
 }
---
+++ CRMWebApi/App_Start/WebApiErrorHandleAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+using CRMWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace CRMWebApi.App_Start
+{
+    public class WebApiErrorHandleAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            LogHelper.Error(actionExecutedContext.Exception);
+            actionExecutedContext.Response = actionExecutedContext.Request.CreateResponse(HttpStatusCode.OK,
+            new AjaxResult<string>()
+            {
+                state = false,
+                error = actionExecutedContext.Exception.Message
+            });
+        }
+    }
+}
